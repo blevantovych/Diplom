fcd86fd7e4679822ebd12fbd315103da4862c61d
bodya17 <levantovychbogdan@gmail.com>
2018-05-20 17:59:50 +0300

    add ability to render formula

--- client/src/components/Formula.js
+++ client/src/components/Formula.js
@@ -1,13 +1,56 @@
-// import MathJax from "react-mathjax";
-import React from 'react';
+/* eslint-disable new-cap */
 
-export default function(props) {
-  return (
-    <div class="formula">
-      {props.formula}
-      {/* <MathJax.Context> */}
-      {/* <MathJax.Node inline></MathJax.Node> */}
-      {/* </MathJax.Context> */}
-    </div>
-  );
+import React, { Component } from 'react'; // we require this to have correct build of component(without error React is not defined)
+import PropTypes from 'prop-types';
+
+export default class TeX extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    showMathMenu: PropTypes.bool,
+    showMathMenuMSIE: PropTypes.bool,
+    style: PropTypes.object,
+    tex2jax: PropTypes.object,
+    formula: PropTypes.string
+  };
+
+  static defaultProps = {
+    showMathMenu: false,
+    showMathMenuMSIE: false,
+    tex2jax: { inlineMath: [['$', '$'], ['\\(', '\\)']] }
+  };
+
+  constructor(props) {
+    super(props);
+
+    MathJax.Hub.Config({
+      tex2jax: props.tex2jax,
+      showMathMenu: props.showMathMenu,
+      showMathMenuMSIE: props.showMathMenuMSIE
+    });
+  }
+
+  componentDidMount() {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.node]);
+  }
+
+  componentDidUpdate() {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.node]);
+  }
+
+  setNode = node => {
+    this.node = node;
+  };
+
+  render() {
+    return (
+      <div
+        className={this.props.className}
+        id="MathJax-TeX"
+        ref={this.setNode}
+        style={this.props.style}
+      >
+        {`$${this.props.formula}$`}
+      </div>
+    );
+  }
 }
