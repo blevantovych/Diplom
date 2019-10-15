7c437a14dd5e2ab47a571cb2e07ae69252c0c81d
blevantovych <levantovychbogdan@gmail.com>
2019-10-15 18:33:15 +0300

    change base url

--- client/api/api-config.js
+++ client/api/api-config.js
@@ -1,52 +1,31 @@
-const MINMAX_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/minmax`
-  : 'https://min-max.herokuapp.com/minmax?';
+const BASE_URL = PRODUCTION
+  ? "https://minmax-approximation.herokuapp.com"
+  : `http://localhost:5000`;
 
-const LSSQ_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/least_squares?`
-  : 'https://min-max.herokuapp.com/least_squares?';
+const MINMAX_URL = `${BASE_URL}/minmax?`;
 
-const LSSQ_DISCRETE_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/least_squares_discrete?`
-  : 'https://min-max.herokuapp.com/least_squares_discrete?';
+const LSSQ_URL = `${BASE_URL}/least_squares?`;
 
-const MINMAX_DISCRETE_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/minmax_discrete?`
-  : 'https://min-max.herokuapp.com/minmax_discrete?';
+const LSSQ_DISCRETE_URL = `${BASE_URL}/least_squares_discrete?`;
 
-const SPLINE_MINMAX_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/spline_minmax?`
-  : 'https://min-max.herokuapp.com/spline_minmax?';
+const MINMAX_DISCRETE_URL = `${BASE_URL}/minmax_discrete?`;
 
-const CONTINUOUS_SPLINE_MINMAX_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/continuous_spline_minmax?`
-  : 'https://min-max.herokuapp.com/spline_minmax?';
+const SPLINE_MINMAX_URL = `${BASE_URL}/spline_minmax?`;
 
-const CONTINUOUS_SPLINE_MINMAX_SEGMENTS_SPECIFIED_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/continuous_spline_minmax_segments_specified?`
-  : 'https://min-max.herokuapp.com/continuous_spline_minmax_segments_specified?';
+const CONTINUOUS_SPLINE_MINMAX_URL = `${BASE_URL}/spline_minmax?`;
 
-const MINMAX_SPLINE_DISCRETE_URL = !PRODUCTION
-  ? `http://${window.location.hostname}:5000/spline_minmax_discrete?`
-  : 'https://min-max.herokuapp.com/spline_minmax_discrete?';
-  ;
+const CONTINUOUS_SPLINE_MINMAX_SEGMENTS_SPECIFIED_URL = `${BASE_URL}/continuous_spline_minmax_segments_specified?`;
+
+const MINMAX_SPLINE_DISCRETE_URL = `${BASE_URL}/spline_minmax_discrete?`;
 // GET
 
-const MINMAX_DISCRETE_URL_GET = !PRODUCTION
-  ? 'http://localhost:5000/minmax_discrete_get_results'
-  : 'https://min-max.herokuapp.com/minmax_discrete_get_results';
+const MINMAX_DISCRETE_URL_GET = `${BASE_URL}/minmax_discrete_get_results`;
 
-const MINMAX_URL_GET = !PRODUCTION
-  ? 'http://localhost:5000/minmax_get_results'
-  : 'https://min-max.herokuapp.com/minmax_get_results';
+const MINMAX_URL_GET = `${BASE_URL}/minmax_get_results`;
 
-const LSSQ_URL_GET = !PRODUCTION
-  ? 'http://localhost:5000/least_squares_get_results'
-  : 'https://min-max.herokuapp.com/least_squares_get_results';
+const LSSQ_URL_GET = `${BASE_URL}/least_squares_get_results`;
 
-const LSSQ_DISCRETE_URL_GET = !PRODUCTION
-  ? 'http://localhost:5000/least_squares_discrete_get_results'
-  : 'https://min-max.herokuapp.com/least_squares_discrete_get_results';
+const LSSQ_DISCRETE_URL_GET = `${BASE_URL}/least_squares_discrete_get_results`;
 
 export {
   MINMAX_URL,
