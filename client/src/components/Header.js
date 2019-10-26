2db9707d39a84c905ccb52d03b38fdf6d1666f2f
blevantovych <levantovychbogdan@gmail.com>
2019-10-26 23:34:17 +0300

    fix issue with link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import AppBar from "material-ui/AppBar";
-import IconButton from "material-ui/IconButton";
-import IconMenu from "material-ui/IconMenu";
-import MenuItem from "material-ui/MenuItem";
-import ArrowDropRight from "material-ui/svg-icons/navigation-arrow-drop-right";
-import { withRouter } from "react-router-dom";
-import { Translate, withLocalize } from "react-localize-redux";
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right'
+import { withRouter } from 'react-router-dom'
+import { Translate, withLocalize } from 'react-localize-redux'
 
-import Hamburger from "material-ui/svg-icons/navigation/menu";
+import Hamburger from 'material-ui/svg-icons/navigation/menu'
 
 const pathNameToTitle = (translate, path) =>
   ({
-    "/ls-discrete": translate("header.ls-discrete"),
-    "/minmax-discrete": translate("header.minmax-discrete"),
-    "/comparison-discrete": translate("header.comparison-discrete"),
-    "/ls": translate("header.ls"),
-    "/minmax": translate("header.minmax"),
-    "/comparison-continuous": translate("header.comparison-continuous"),
-    "/spline": translate("header.spline"),
-    "/continuous-spline": translate("header.continuous-spline"),
-    "/continuous-spline-segments-specified": translate(
-      "header.continuous-spline-segments-specified"
+    '/ls-discrete': translate('header.ls-discrete'),
+    '/minmax-discrete': translate('header.minmax-discrete'),
+    '/comparison-discrete': translate('header.comparison-discrete'),
+    '/ls': translate('header.ls'),
+    '/minmax': translate('header.minmax'),
+    '/comparison-continuous': translate('header.comparison-continuous'),
+    '/spline': translate('header.spline'),
+    '/continuous-spline': translate('header.continuous-spline'),
+    '/continuous-spline-segments-specified': translate(
+      'header.continuous-spline-segments-specified'
     ),
-    "/spline-minmax-discrete": translate("header.spline-minmax-discrete")
-  }[path]);
+    '/spline-minmax-discrete': translate('header.spline-minmax-discrete')
+  }[path])
 
 const Menu = props => (
   <Translate>
@@ -31,71 +31,71 @@ const Menu = props => (
       <IconMenu
         iconButtonElement={
           <IconButton>
-            <Hamburger color={"white"} />
+            <Hamburger color={'white'} />
           </IconButton>
         }
-        targetOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "top" }}
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
       >
         <MenuItem
-          primaryText={translate("menu.discrete_case")}
+          primaryText={translate('menu.discrete_case')}
           rightIcon={<ArrowDropRight />}
           menuItems={[
             <MenuItem
               key="МНК_дискр"
-              primaryText={translate("menu.ls")}
-              onClick={() => props.history.push("/ls-discrete")}
+              primaryText={translate('menu.ls')}
+              onClick={() => props.history.push('/ls-discrete')}
             />,
             <MenuItem
               key="Мінімакс_дискр"
-              primaryText={translate("menu.minimax")}
-              onClick={() => props.history.push("/minmax-discrete")}
+              primaryText={translate('menu.minimax')}
+              onClick={() => props.history.push('/minmax-discrete')}
             />,
             <MenuItem
               key="Порівняти_дискр"
-              primaryText={translate("menu.compare")}
-              onClick={() => props.history.push("/comparison-discrete")}
+              primaryText={translate('menu.compare')}
+              onClick={() => props.history.push('/comparison-discrete')}
             />
           ]}
         />
         <MenuItem
-          primaryText={translate("menu.continuous")}
+          primaryText={translate('menu.continuous')}
           rightIcon={<ArrowDropRight />}
           menuItems={[
             <MenuItem
               key="МНК"
-              primaryText={translate("menu.ls")}
-              onClick={() => props.history.push("/ls")}
+              primaryText={translate('menu.ls')}
+              onClick={() => props.history.push('/ls')}
             />,
             <MenuItem
               key="Мінімакс"
-              primaryText={translate("menu.minimax")}
-              onClick={() => props.history.push("/minmax")}
+              primaryText={translate('menu.minimax')}
+              onClick={() => props.history.push('/minmax')}
             />,
             <MenuItem
               key="Порівняти"
-              primaryText={translate("menu.compare")}
-              onClick={() => props.history.push("/comparison-continuous")}
+              primaryText={translate('menu.compare')}
+              onClick={() => props.history.push('/comparison-continuous')}
             />
           ]}
         />
         <MenuItem
-          primaryText={translate("menu.spline_approximation")}
-          onClick={() => props.history.push("/spline")}
+          primaryText={translate('menu.spline_approximation')}
+          onClick={() => props.history.push('/spline')}
         />
         <MenuItem
-          primaryText={translate("menu.continuous_spline_approximation")}
-          onClick={() => props.history.push("/continuous-spline")}
+          primaryText={translate('menu.continuous_spline_approximation')}
+          onClick={() => props.history.push('/continuous-spline')}
         />
         <MenuItem
-          primaryText={translate("menu.points_specified")}
+          primaryText={translate('menu.points_specified')}
           onClick={() =>
-            props.history.push("/continuous-spline-segments-specified")
+            props.history.push('/continuous-spline-segments-specified')
           }
         />
         <MenuItem
-          primaryText={translate("menu.minmax_spline_discrete")}
-          onClick={() => props.history.push("/spline-minmax-discrete")}
+          primaryText={translate('menu.minmax_spline_discrete')}
+          onClick={() => props.history.push('/spline-minmax-discrete')}
         />
         {/*<MenuItem
             primaryText="Історія"
@@ -107,23 +107,22 @@ const Menu = props => (
       </IconMenu>
     )}
   </Translate>
-);
+)
 // withLocalize(LanguageToggle)
 
 const LanguageToggle = withLocalize(({ languages, setActiveLanguage }) => (
   <div>
     {languages.map(lang => (
-      <a
-        style={{ textDecoration: "none", marginLeft: "10px", color: "white" }}
+      <span
+        style={{ cursor: 'pointer', marginLeft: '10px', color: 'white' }}
         key={lang.code}
-        href="#"
         onClick={() => setActiveLanguage(lang.code)}
       >
         {lang.name}
-      </a>
+      </span>
     ))}
   </div>
-));
+))
 class Header extends Component {
   render() {
     return (
@@ -137,8 +136,8 @@ class Header extends Component {
               iconElementRight={
                 <div
                   style={{
-                    display: "flex",
-                    alignItems: "center"
+                    display: 'flex',
+                    alignItems: 'center'
                   }}
                 >
                   <LanguageToggle />
@@ -152,8 +151,8 @@ class Header extends Component {
           </div>
         )}
       </Translate>
-    );
+    )
   }
 }
 
-export default withRouter(Header);
+export default withRouter(Header)
